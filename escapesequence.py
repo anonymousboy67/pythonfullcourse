635ca1d79e772a2cad4db7a8e28a6933e076fc7e
anonymousboy67 <aashishad67@gmail.com>
2025-07-09 11:38:26 +0545

    formatted strings

---
+++ escapesequence.py
@@ -0,0 +1,16 @@
+# \"
+
+# \n -for new line
+# \\ for one \
+course = "Python \"Programming"
+print(course)
+
+
+# concationation
+
+first = "Aashish"
+last = "Adhikari"
+
+full = first + "" + last
+full = f"{first} {last}"
+print(full)
