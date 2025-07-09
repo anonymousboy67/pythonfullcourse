a21c51939161dff73609688a4c85717497aaeb00
anonymousboy67 <aashishad67@gmail.com>
2025-07-09 11:44:26 +0545

    string methods

---
+++ string_methods.py
@@ -0,0 +1,12 @@
+
+# everthing in python is an object and object has function called method which we can access using . notation
+
+course = "Python program"
+print(course.upper())
+
+print(course.lower())
+print(course.title())
+print(course.strip())
+print(course.find("pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
